a56b705586c0497bd1e2eae7dca515959b030d25
equalsum <38664511+equalsum@users.noreply.github.com>
2021-11-22 04:09:48 +0300

    Update Soldagotchi.py
    Updated to Python3

--- Soldagotchi.py
+++ Soldagotchi.py
@@ -70,15 +70,15 @@ while True:
     for p in players["Players"]:
         tot = tot + 1
         tempplayers.append(p)
-    print server_info["Name"]+" "+"["+str(tot)+"]:"
-    print GetSpace(5)+"│"
-    print GetSpace(5)+"│"
-    print GetSpace(5)+server_info["CurrentMap"]+":"
-    print GetSpace(10)+"│"
-    print GetSpace(10)+"│"
-    print GetSpace(10)+"Players:"
+    print (server_info["Name"]+" "+"["+str(tot)+"]:")
+    print (GetSpace(5)+"│")
+    print (GetSpace(5)+"│")
+    print (GetSpace(5)+server_info["CurrentMap"]+":")
+    print (GetSpace(10)+"│")
+    print (GetSpace(10)+"│")
+    print (GetSpace(10)+"Players:")
     for pl in tempplayers:
-        print GetSpace(15)+pl+GetSpace(22-len(pl))+" ["+GetCountry(pl)+"]"
+        print (GetSpace(15)+pl+GetSpace(22-len(pl))+" ["+GetCountry(pl)+"]"))
         
     if (initialized == False):
         lasttot = tot
@@ -88,33 +88,33 @@ while True:
         print "\n"
         if (tot > lasttot):
             diffplayers = [player for player in tempplayers if player not in lastplayers]
-            print "Following player has joined the game:\n"
+            print ("Following player has joined the game:\n")
             for player in diffplayers:
-                print "    " + player
+                print ("    " + player)
         elif (tot < lasttot):
             diffplayers = [player for player in lastplayers if player not in tempplayers]
-            print "Following player has left the game:\n"
+            print ("Following player has left the game:\n")
             for player in diffplayers:
-                print "    " + player
+                print ("    " + player)
             
         if tot < server_info["MaxPlayers"]:
             if (random.randrange(1,5) == 3):
-                print "A slot is avaliable, you can join! "
-                print happy3
+                print ("A slot is avaliable, you can join! ")
+                print (happy3)
             elif (random.randrange(1,5) == 2):
-                print "A slot is avaliable, you can join! "
-                print happy2
+                print ("A slot is avaliable, you can join! ")
+                print (happy2)
             else:
-                print "A slot is avaliable, you can join! "
-                print happy1
+                print ("A slot is avaliable, you can join! ")
+                print (happy1)
         elif (random.randrange(1,5) == 3):
-            print "It's full..."
-            print sad3
+            print ("It's full...")
+            print (sad3)
         elif (random.randrange(1,5) == 2):
-            print "It's full..."
-            print sad2
+            print ("It's full...")
+            print (sad2)
         else:
-            print "It's full..."
-            print sad1
+            print ("It's full...")
+            print (sad1)
         lasttot = tot
         lastplayers = tempplayers
